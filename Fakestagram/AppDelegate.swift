5625faa0630f820c9d9e72ff14976f482ff8c28c
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-03-22 20:22:13 -0600

    fist commit

--- Fakestagram/AppDelegate.swift
+++ Fakestagram/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    // Aqui en esta funcion didFinishLaunchingWithOptions es antes de que se termine de finalizar la app
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
